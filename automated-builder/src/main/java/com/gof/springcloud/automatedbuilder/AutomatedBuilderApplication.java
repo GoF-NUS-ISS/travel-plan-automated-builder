b9be650e2d54203339388f2677364f06f2e91102
timothy <timhou@outlook.com>
2020-09-08 17:34:14 +0800

    add new project

---
+++ automated-builder/src/main/java/com/gof/springcloud/automatedbuilder/AutomatedBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.gof.springcloud.automatedbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatedBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomatedBuilderApplication.class, args);
+	}
+
+}
