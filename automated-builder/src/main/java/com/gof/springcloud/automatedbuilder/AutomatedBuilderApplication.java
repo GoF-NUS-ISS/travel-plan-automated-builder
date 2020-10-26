597d854f1302cadd025ecbf01b5428901d75f629
timothy <timhou@outlook.com>
2020-10-26 14:03:52 +0800

    remove eureka

--- automated-builder/src/main/java/com/gof/springcloud/automatedbuilder/AutomatedBuilderApplication.java
+++ automated-builder/src/main/java/com/gof/springcloud/automatedbuilder/AutomatedBuilderApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
@@ -15,8 +15,8 @@ import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableEurekaClient //Register into eureka after initiation
-@EnableDiscoveryClient
+//@EnableEurekaClient //Register into eureka after initiation
+//@EnableDiscoveryClient
 @EnableCircuitBreaker //import hystrix for fusing mechanism
 @EnableCaching
 public class AutomatedBuilderApplication {
